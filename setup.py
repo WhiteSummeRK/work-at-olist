91e02f9e845eb229f523b407b7fac88815618318
WhiteSummeRK <kauan.alves1996@hotmail.com>
2019-05-31 00:40:47 -0300

    Adding setup.py with description parameters

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='call receiver',
+    version='0.0.10',
+    description='',
+    url='https://github.com/WhiteSummeRK/work-at-olist',
+    author='Kauan Alves',
+    author_email='kauan.alves1996@hotmail.com',
+    license='MIT',
+    packages=['call_receiver'],
+    zip_safe=False,
+)
